ba7391b76637f6cc9d4f4970c3d3a6afaabe4a05
Maja Graonic <m.graonic@gmail.com>
2018-06-18 15:25:59 -0700

    one route working

---
+++ src/components/Movies.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+class Movies extends Component {
+  render(){
+    return(
+      <h2>Your f**ing movies</h2>
+    )
+  }
+}
+
+export default Movies;
---
+++ src/components/Search.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+export default Search;
