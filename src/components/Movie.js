93f4487bac5389e42cbe7323b46d601692e59046
Maja Graonic <m.graonic@gmail.com>
2018-06-19 14:48:20 -0700

    testing if changes go up

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class Movies extends Component {
-
+// this is a change
   buttonClick = () => {
     let movie = {
       title: this.props.movieTitle,
